3cdd7e87fa2f9bcf0d2ce0c183c628f507166016
Mike Mclaughlin <mike-mclaughlin@mikes-mbp.lan>
2023-03-29 15:44:09 -0500

    master updated vioski page with appropriate links in data

---
+++ src/utils/objects/links/code-links-vioski.js
@@ -0,0 +1,49 @@
+export const codeVioski = [
+    {
+        text: 'Vioski Repository',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io',
+        explanation: 'This is the entire repository for the prototype',
+    },
+    {
+        text: 'Vioski Main index.js',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/index.js',
+        explanation:
+            'This is the main website code that I developed using jQuery, CSS3 and HTML5.',
+    },
+    {
+        text: 'Homepage Items Code',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/homepageSession.js',
+        explanation:
+            'This is the homepage items code for all of the individual items on the page. This was done using jQuery.',
+    },
+    {
+        text: 'Google api code for the Rep Locator page',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/reps.js',
+        explanation:
+            'This is the Google api representative object I wrote for all of the representatives globally.',
+    },
+    {
+        text: 'Google api gKey code',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/gkey.js',
+        explanation:
+            'This is the Google api gKey code that helps render the api markers for the rep locator. I wrote this in jQuery.',
+    },
+    {
+        text: 'Local Storage',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/session.js',
+        explanation:
+            'This is the localStorage code I used to parse the JSON for the the Item Details page.  I wrote this with jQuery',
+    },
+    {
+        text: 'Filtering code for the Collection pages',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/scripts/dynamicFilter.js',
+        explanation:
+            'This is the filtering code I wrote to supply to the user the ability to filter through the entire collection based on type.  This was done using jQuery.',
+    },
+    {
+        text: 'Styles for the website',
+        href: 'https://github.com/michael-mclaughlin/vioski.github.io/blob/master/styles.css',
+        explanation:
+            'This is all of the styles related to the website.  This was done in pure CSS3.',
+    },
+];
--- src/vioskiPage.tsx
+++ src/vioskiPage.tsx
@@ -5,22 +5,22 @@ import { HeaderFlex } from './components/layout/header-flex';
 import { DivContainerFlex } from './components/layout/div-container-flex';
 import { Link as Links } from './components/elements/link';
 import { links } from './utils/objects/links/links';
-// import { Grid } from './components/layout/grid-container';
-// import { codeVioski } from './utils/objects/links/code-links-vioski';
+import { Grid } from './components/layout/grid-container';
+import { codeVioski } from './utils/objects/links/code-links-vioski';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // import vioskiVideo from './videos/vioski-video.mp4';
 import './scss/App.scss';
 import './scss/material-ui-overrides.scss';
-// import { Card } from "./components/composed/card";
+import { Card } from "./components/composed/card";
 
 const App = () => {
-    // interface codeLinksVioski {
-    //     text: string;
-    //     href: string;
-    //     explanation: string;
-    // }
+    interface codeLinksVioski {
+        text: string;
+        href: string;
+        explanation: string;
+    }
     return (
         <AppContainer className="App">
             <HeaderFlex
@@ -185,7 +185,7 @@ const App = () => {
                         Code Samples
                     </h2>
                     <p className="code-sample-text-explanation">Here is some of the code that I wrote for this website. I believe in open sourced code sharing so please if you see something you like please don't hesitate to copy the code and Enjoy!</p>
-                        {/* <DivContainerFlex
+                        <DivContainerFlex
                             className="about-me-text-container"
                             styleProps={{ flexFlow: 'column', width: '100%', marginBottom: '2.5rem' }}
                         >
@@ -214,7 +214,7 @@ const App = () => {
                                     )
                                 })}
                             </Grid>
-                        </DivContainerFlex> */}
+                        </DivContainerFlex>
                     </DivContainerFlex>
                 </DivContainerFlex>
                 <DivContainerFlex className="go-to-home-link-container bottom-page-link">
